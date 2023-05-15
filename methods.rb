98b3787faa515645f4c8dde81e40e26191dfc364
iamkinglana <ngetichenock32@gmail.com>
2023-05-15 23:37:59 +0300

    First Commit

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name ="Naureen")
+    puts "Hello, #{name}!"
+end
+greet
+# greet("Naureen")
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (num1,num2)
+    return num1 + num2  
+end
+#Add
+
+
+def halve(number)
+    if number.is_a?(Integer)
+        return number / 2
+    else 
+        return nil
+    end
+end
